096de3b203e1c1fe5eec91819a7a375809efd4c9
Minyoung Shin <mins6649@gmail.com>
2021-02-02 22:52:08 -0500

    o

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
 # Your Code Here
+def map(array)
+    array.map {|x| yield(x)}
+end
+
+def reduce(array, starting_point=0)
+    array.reduce(starting_point) {|starting_point,y| yield(starting_point,y)}
+end
